d1206dadf8c50f2765c117a5fef38e9619ed3242
Yassmine-hsaini <yassmina49hsaini@gmail.com>
2024-01-08 11:08:10 +0100

    retouche

--- config-server/src/main/java/com/ensa/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/ensa/configserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.configserver;
+package com.ensa.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
